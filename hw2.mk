fcba048c385e0f2e6bde5c9abb7094f075c9ea48
Default user <vmartinita10@hotmail.com>
2019-04-20 22:07:05 +0000

    mk_file

---
+++ hw2.mk
@@ -0,0 +1,16 @@
+Resultados_hw2.pdf : resultados.tex signals.pdf Fourier_trans.pdf Temblor.pdf Fourier_temblor.pdf plot.pdf
+    pdflatex resultados.tex
+signals.pdf: Fourier.py
+    python Fourier.py
+Fourier_trans.pdf: Fourier.py
+    python Fourier.py
+Temblor.pdf: Fourier.py
+    python Fourier.py
+Fourier_temblor.pdf: Fourier.py
+    python Fourier.py
+plot.pdf : Plots_hw2.py datos.dat
+    python plot_hw2.py
+datos.dat : a.out
+    ./a.out >> datos. dat
+a.out : makedatos.cpp
+    g++ makedatos.cpp
