1f640682a338e2a0bb943e87c52c59f076f12f92
Default user <vmartinita10@hotmail.com>
2019-05-08 05:25:15 +0000

    archivos

--- Edificio.cpp
+++ Edificio.cpp
@@ -4,9 +4,12 @@
 #include <math.h>
 using namespace std;
 void leapfrog(float inicial, float finali, float dt);
+
+void otro(float inicial, float finali, float dt);
 int main()
 {
     leapfrog(0.0, 20.0, 0.1);
+    otro(0.0, 20.0, 0.1);
     return(0); 
 }
 
@@ -96,23 +99,35 @@ void leapfrog(float inicial, float finali, float dt)
                 V1viejo= V1viejo-(dt/2)*(1/m)*((-gamma*V1viejo)-(2*k*U1viejo)+(k*U2viejo)+sin(omegas[contador2]*inicial));
                 V1nuevo= V1viejo+dt*(1/m)*((-gamma*V1viejo)-(2*k*U1viejo)+(k*U2viejo)+sin(omegas[contador2]*inicial));
                 U1nuevo= U1viejo+V1nuevo*dt;
-                if(U1nuevo>U1viejo)
+                if(abs(U1nuevo)>U1viejo)
                 {
                     posicion1[contador2]=U1nuevo;
+                }
+                else
+                {
+                    posicion1[contador2]=U2viejo;
                 }
                 V2viejo= V2viejo-(dt/2)*(1/m)*((-gamma*V2viejo)+(k*U1viejo)-(2*k*U2viejo)+(k*U3viejo));
                 V2nuevo=V2viejo+dt*(1/m)*((-gamma*V2viejo)+(k*U1viejo)-(2*k*U2viejo)+(k*U3viejo));
                 U2nuevo=U2viejo+V2nuevo*dt;
-                if(U2nuevo>U2viejo)
+                if(abs(U2nuevo)>U2viejo)
                 {
                     posicion2[contador2]=U2nuevo;
+                }
+                else
+                {
+                    posicion2[contador2]=U2viejo;
                 }
                 V3viejo= V3viejo-(dt/2)*(1/m)*((-gamma*V3viejo)+(k*U2viejo)-(k*U3viejo));
                 V3nuevo= V3viejo+dt*(1/m)*((-gamma*V3viejo)+(k*U2viejo)-(k*U3viejo));
                 U3nuevo=U3viejo+V3nuevo*dt;
-                if(U3nuevo>U3viejo)
+                if(abs(U3nuevo)>U3viejo)
                 {
                     posicion3[contador2]=U3nuevo;
+                }
+                else
+                {
+                    posicion3[contador2]=U3viejo;
                 }
 
                 j=j+1;
@@ -130,3 +145,46 @@ void leapfrog(float inicial, float finali, float dt)
 
 }
 
+void otro(float inicial, float finali, float dt)
+{
+    float V1nuevo=0;
+    float U1nuevo=0;
+    float V2nuevo=0;
+    float U2nuevo=0;
+    float V3nuevo=0;
+    float U3nuevo=0;
+    float m=1000;
+    float gamma=0;
+    float k=2000;
+    float omega= 3.7;
+    float V1viejo;
+    float U1viejo;
+    float V2viejo;
+    float U2viejo;
+    float V3viejo;
+    float U3viejo;
+    ofstream outfile;
+    outfile.open("datos37.dat");
+    while (inicial<finali)
+    {
+        V1viejo=V1nuevo;
+        U1viejo=U1nuevo;
+        V2viejo=V2nuevo;
+        U2viejo=U2nuevo;
+        V3viejo=V3nuevo;
+        U3viejo=U3nuevo;
+        outfile << U1nuevo <<" "<< U2nuevo <<" "<< U3nuevo <<" "<< inicial <<endl;
+        V1viejo= V1viejo-(dt/2)*(1/m)*((-gamma*V1viejo)-(2*k*U1viejo)+(k*U2viejo)+sin(omega*inicial));
+        V1nuevo= V1viejo+dt*(1/m)*((-gamma*V1viejo)-(2*k*U1viejo)+(k*U2viejo)+sin(omega*inicial));
+        U1nuevo= U1viejo+V1nuevo*dt;
+        V2viejo= V2viejo-(dt/2)*(1/m)*((-gamma*V2viejo)+(k*U1viejo)-(2*k*U2viejo)+(k*U3viejo));
+        V2nuevo=V2viejo+dt*(1/m)*((-gamma*V2viejo)+(k*U1viejo)-(2*k*U2viejo)+(k*U3viejo));
+        U2nuevo=U2viejo+V2nuevo*dt;
+        V3viejo= V3viejo-(dt/2)*(1/m)*((-gamma*V3viejo)+(k*U2viejo)-(k*U3viejo));
+        V3nuevo= V3viejo+dt*(1/m)*((-gamma*V3viejo)+(k*U2viejo)-(k*U3viejo));
+        U3nuevo=U3viejo+V3nuevo*dt;
+        inicial= inicial+dt;
+          
+    }
+    outfile.close();
+}
